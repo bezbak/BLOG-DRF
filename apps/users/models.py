a323248bbff657ecc5333d5298c53c7815cb7cd3
bezbak <Cemetey.2019@gmail.com>
2023-06-12 16:11:40 +0600

    first commit

---
+++ apps/users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class User(AbstractUser):
+    date_of_birth = models.DateTimeField(
+        auto_now_add=False
+    )
+    proflie_image = models.ImageField(
+        upload_to='proflie_image/'
+    )
+    description = models.TextField(
+        max_length=555
+    )
+    
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
